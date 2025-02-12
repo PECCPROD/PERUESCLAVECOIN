81d9c4ab678aadfa121bc61073087a37aa861b12
PECCPROD <peruesclavecoin@gmail.com>
2025-02-11 20:37:31 -0500

    Subiendo github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://PECCPROD.github.io',
-  base: 'PERUESCLAVECOIN',
+  base: '/PERUESCLAVECOIN/',
   vite: {
     plugins: [tailwindcss()]
   }
